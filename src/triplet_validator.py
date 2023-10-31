f1246b032e747f58b2bd5907a3a993d331f62004
southeo <soulaine.theo@gmail.com>
2023-10-31 14:09:36 +0100

    pass whole triplet

--- src/triplet_validator.py
+++ src/triplet_validator.py
@@ -4,25 +4,19 @@ import csv
 sah_endpoint = "https://www.ebi.ac.uk/ena/sah/api/validate"
 
 
-def validate_triplet(org, collection, catalog_id, ggbn_id):
+def validate_triplet(triplet, ggbn_id):
     params = {
-        "value": encode_triplet(org, collection, catalog_id),
+        "value": triplet,
         "qualifier_type": "specimen_voucher"
     }
     r = requests.get(sah_endpoint, params).json()
-    return capture_errors(r, org, collection, catalog_id, ggbn_id)
+    return capture_errors(r, triplet, ggbn_id)
 
 
-def capture_errors(results, org, collection, catalog_id, ggbn_id):
+def capture_errors(results, triplet, ggbn_id):
     if not results["success"]:
         with open("tripletErrors.csv", 'a+') as error_file:
             writer = csv.writer(error_file)
-            writer.writerow([org, collection, catalog_id, ggbn_id])
+            writer.writerow([triplet, ggbn_id])
         return False
     return True
-
-
-def encode_triplet(org, collection, catalog_id):
-    if collection:
-        return org + ":" + collection + ":" + catalog_id
-    return org + ":" + catalog_id
