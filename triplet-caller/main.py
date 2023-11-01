9a044a4a37224f764126a149e19ddca40c4090b8
southeo <soulaine.theo@gmail.com>
2023-11-01 12:23:58 +0100

    triplet caller

--- triplet-caller/main.py
+++ triplet-caller/main.py
@@ -54,16 +54,7 @@ def validate_triplet(triplet, ggbn_id):
         "qualifier_type": "specimen_voucher"
     }
     r = requests.get(sah_endpoint, params).json()
-    return capture_errors(r, triplet, ggbn_id)
-
-
-def capture_errors(results, triplet, ggbn_id):
-    if not results["success"]:
-        with open("tripletErrors.csv", 'a+') as error_file:
-            writer = csv.writer(error_file)
-            writer.writerow([triplet, ggbn_id])
-        return False
-    return True
+    return r["success"]
 
 
 def build_triplet(row, include_collection):
